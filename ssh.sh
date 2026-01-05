3ba7150ec44966d5fb5c5d20293f1dd33ee96275
Hemanths-git <hemanthgowda0415@gmail.com>
2026-01-05 11:21:20 +0000

    ssh example

---
+++ ssh.sh
@@ -0,0 +1,7 @@
+example for ssh
+in order to create ssh key pass ssh-keygen command
+check that key in ~/.ssh location like la -la ~/.ssh
+if you want to generate key in diff algorithm just do ssh-keygen -t dsa , it can be dsa , rsa ,sha types , default is rsa
+	once created add this public key in settings git hub ssh key
+	once done authentic server to git@github.com /enterprise edition
+then add ssh url to local git add surl repo
